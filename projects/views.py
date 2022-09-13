142b5479b5fb43152be616172927d26d1555e326
Brooke Vonderheid <bevfrisco@gmail.com>
2022-09-13 16:51:26 -0400

    Featur 8 complete

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 from projects.models import Project
@@ -7,6 +8,9 @@ from projects.models import Project
 # Create your views here.
 
 
-class ProjectListView(ListView):
+class ProjectListView(LoginRequiredMixin, ListView):
     model = Project
     template_name = "projects/projects.html"
+
+    def get_queryset(self):
+        return Project.objects.filter(members=self.request.user)
