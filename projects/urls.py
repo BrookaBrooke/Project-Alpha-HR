9db789f7b1229c4ba5e16ca312d1900c95e90bc3
Brooke Vonderheid <bevfrisco@gmail.com>
2022-09-14 15:27:58 -0400

    feature 13 halfway there

--- projects/urls.py
+++ projects/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
-from projects.views import ProjectListView
+from projects.views import ProjectListView, ProjectDetailView
 
-urlpatterns = [path("", ProjectListView.as_view(), name="list_projects")]
+urlpatterns = [
+    path("", ProjectListView.as_view(), name="list_projects"),
+    path("", ProjectDetailView.as_view(), name="show_project"),
+]
--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from django.views.generic.detail import DetailView
 
 from projects.models import Project
+
 
 # Create your views here.
 
@@ -11,6 +12,14 @@ from projects.models import Project
 class ProjectListView(LoginRequiredMixin, ListView):
     model = Project
     template_name = "projects/projects.html"
+
+    def get_queryset(self):
+        return Project.objects.filter(members=self.request.user)
+
+
+class ProjectDetailView(LoginRequiredMixin, DetailView):
+    model = Project
+    template_name = "projects/show_project.html"
 
     def get_queryset(self):
         return Project.objects.filter(members=self.request.user)
