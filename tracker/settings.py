9a8eec65ed1cf06a15ffd999b7b5ebe3f1106202
Brooke Vonderheid <bevfrisco@gmail.com>
2022-09-15 12:32:28 -0400

    added feature 18

--- tracker/settings.py
+++ tracker/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "markdownify.apps.MarkdownifyConfig",
     "tasks.apps.TasksConfig",
     "projects.apps.ProjectsConfig",
     "accounts.apps.AccountsConfig",
@@ -129,3 +130,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MARKDOWNIFY = {"default": {"BLEACH": False}}
