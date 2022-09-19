0ea86d16f8b782ab17f9160a2689a769c0f4df3c
Brooke Vonderheid <bevfrisco@gmail.com>
2022-09-19 18:58:06 -0400

    Feature 11 complete

--- tasks/models.py
+++ tasks/models.py
@@ -7,8 +7,8 @@ from django.contrib.auth.models import User
 
 class Task(models.Model):
     name = models.CharField(max_length=200)
-    start_date = models.DateTimeField(null=True, blank=True)
-    due_date = models.DateTimeField(null=True, blank=True)
+    start_date = models.DateTimeField()
+    due_date = models.DateTimeField()
     is_completed = models.BooleanField(default=False)
     project = models.ForeignKey(
         Project,
