82042f308e013e0a63c81c629cc6eece585f9c35
Brooke Vonderheid <bevfrisco@gmail.com>
2022-09-20 16:53:48 -0400

    fixed bug on 17

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,5 +5,5 @@ from tasks.views import TaskCreateView, TaskListView, TaskUpdateView
 urlpatterns = [
     path("create/", TaskCreateView.as_view(), name="create_task"),
     path("mine/", TaskListView.as_view(), name="show_my_tasks"),
-    path("<int:pk>/complete", TaskUpdateView.as_view(), name="complete_task"),
+    path("<int:pk>/complete/", TaskUpdateView.as_view(), name="complete_task"),
 ]
